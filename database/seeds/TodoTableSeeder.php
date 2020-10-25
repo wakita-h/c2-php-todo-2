01bb9781c2b139580d106e4657e8f0d05231d8d6
Task Nagashige <task.19.xx@gmail.com>
2020-10-25 18:20:50 +0900

    seederの更新

--- database/seeds/TodoTableSeeder.php
+++ database/seeds/TodoTableSeeder.php
@@ -14,11 +14,22 @@ class TodoTableSeeder extends Seeder
         for($i = 1; $i <= 100; $i++){
             DB::table('todos')->insert([
                 'title' => "タスク$i",
+                'user_id' => self::createUserIdByTaskId($i),
                 'due_date' => date('Y-m-d'),
                 'status' => 0,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s'),
             ]);
         }
+    }
+
+    /**
+     * タスクidからユーザーidを生成する
+     * @param int $task_id
+     * @return int
+     */
+    private function createUserIdByTaskId(int $task_id): int
+    {
+        return ceil($task_id*0.1);
     }
 }
