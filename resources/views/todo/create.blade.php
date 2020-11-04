ed472445b8ea2ce859a2db134c803005ee067fda
Task Nagashige <task.19.xx@gmail.com>
2020-11-05 02:36:13 +0900

    POSTするURLの変更

--- resources/views/todo/create.blade.php
+++ resources/views/todo/create.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="col-md-10">
             <h2 class="text-muted py-3">Todo 作成</h2>
-            <form action="/todo/store" method="POST">
+            <form action="/todo" method="POST">
                 @include('parts.todo.input')
                 <button class="btn btn-primary" type="submit">作成</button>
             </form>
