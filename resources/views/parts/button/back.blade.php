7a0eddda11de69b07e8399faa3f0e090c89700b1
Task Nagashige <task.19.xx@gmail.com>
2020-11-05 02:40:22 +0900

    戻るボタンの挙動修正

--- resources/views/parts/button/back.blade.php
+++ resources/views/parts/button/back.blade.php
@@ -1,1 +1,1 @@
-<button class="btn btn-primary" type="button" onclick="history.back()"><i class="fas fa-chevron-left mr-2"></i>戻る</button>
+<a href="/todo" class="btn btn-primary"><i class="fas fa-chevron-left mr-2"></i>戻る</a>
