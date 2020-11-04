361169376adfee289c8a28b66256ad06f08f1ae3
Task Nagashige <task.19.xx@gmail.com>
2020-11-04 23:59:44 +0900

    戻るボタンを共通パーツ化した

---
+++ resources/views/parts/button/back.blade.php
@@ -0,0 +1,1 @@
+<button class="btn btn-primary" type="button" onclick="history.back()"><i class="fas fa-chevron-left mr-2"></i>戻る</button>
--- resources/views/todo/show.blade.php
+++ resources/views/todo/show.blade.php
@@ -18,6 +18,6 @@
             </tr>
             </tbody>
         </table>
-        <button class="btn btn-primary" type="button" onclick="history.back()">戻る</button>
+        @include('parts.button.back')
     </div>
 @endsection
