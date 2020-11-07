a1bd40cefe6c8e097c2feca777fafc054349a7f0
Task Nagashige <task.19.xx@gmail.com>
2020-11-07 17:46:11 +0900

    編集内容を更新できるようにした

--- resources/views/parts/todo/input.blade.php
+++ resources/views/parts/todo/input.blade.php
@@ -4,7 +4,7 @@
 </div>
 <div class="form-group">
     <label for="due_date">期限</label>
-    <input type="date" class="form-control" name="due_date" placeholder="2020/10/31" value="{{ $todo && $todo->due_date ? $todo->due_date: '' }}" required>
+    <input type="date" class="form-control" name="due_date" placeholder="2020-10-31" value="{{ $todo && $todo->due_date ? $todo->due_date: '' }}" required>
 </div>
 <input type="hidden" name="_token" value="{{ csrf_token() }}">
 @if ($errors->any())
