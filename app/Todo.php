27f6f0294d6d5fc5fa42d8558acd975a56ed7938
Task Nagashige <task.19.xx@gmail.com>
2020-12-08 23:31:39 +0900

    [Fix] fix error

--- app/Todo.php
+++ app/Todo.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     const STATUS_NOT_YET = 0;
+    const STATUS = [
+        '未対応',
+        '作業中',
+        '完了',
+    ];
 
     protected $fillable = ['title', 'due_date', 'status'];
 }
