99b7748d24cf9274fecaf857256c9791adeb69bc
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-12-09 10:08:40 +0900

    commit

--- app/Todo.php
+++ app/Todo.php
@@ -7,6 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     const STATUS_NOT_YET = 0;
+    const STATUS = [
+        '未対応',
+        '作業中',
+        '完了',
+    ];
 
     protected $fillable = ['title', 'due_date', 'status'];
+
+    /**
+     * 状態の表示テキストを返す
+     *
+     * @return string
+     */
+    public function getStatusText(): string
+    {
+        if (empty(self::STATUS[$this->status])) {
+            return  "";
+        }
+
+        return self::STATUS[$this->status];
+    }
 }
--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -10,8 +10,9 @@
             <table class="table">
                 <thead>
                 <tr>
-                    <th width="50%">タイトル</th>
+                    <th width="40%">タイトル</th>
                     <th width="20%">期限</th>
+                    <th width="10%">状態</th>
                     <th width="15%"></th>
                     <th width="15%"></th>
                 </tr>
@@ -25,6 +26,9 @@
                             </a>
                         </td>
                         <td>{{ $todo->due_date }}</td>
+                        <td>
+                            {{ $todo->getStatusText() }}
+                        </td>
                         <th>
                             <a href="/todo/{{ $todo->id }}/edit" class="btn btn-success"><i class="fas fa-edit mr-2"></i>編集</a>
                         </th>
