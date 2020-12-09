dfa3e373d10e71ccda5faf9ac568ed80ed48adbb
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-12-09 10:23:43 +0900

    z

--- resources/views/parts/todo/input.blade.php
+++ resources/views/parts/todo/input.blade.php
@@ -6,6 +6,21 @@
     <label for="due_date">期限</label>
     <input type="date" class="form-control" name="due_date" placeholder="2020-10-31" value="{{ $todo && $todo->due_date ? $todo->due_date: '' }}" required>
 </div>
+
+<div class="form-group">
+    <label for="status">状態</label>
+    <select name="status" id="status" class="form-control">
+        @foreach(\App\Todo::STATUS as $key => $val)
+        <option
+            value="{{ $key }}"
+            {{ $key == old('status', $todo->status) ? 'selected' : ''}}
+        >
+            {{ $val }}
+        </option>
+        @endforeach
+    </select>
+</div>
+
 @csrf
 @if ($errors->any())
     <div class="alert alert-danger">
